8e534819c9f8a2d0148309f5ad10e91f724ddb9b
Misery <senya.stoletov.90@mail.ru>
2024-07-25 16:12:17 +0500

    updated the In Memory History Manager class to use a doubly linked list

---
+++ src/InMemoryHistoryManager.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private final Map<Integer, Node<Task>> taskNodeMap = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+
+    @Override
+    public void add(Task task) {
+        remove(task.getId());
+        linkLast(task);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = taskNodeMap.remove(id);
+        if (node != null) {
+            removeNode(node);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        List<Task> history = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            history.add(current.item);
+            current = current.next;
+        }
+        return history;
+    }
+
+    private void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        taskNodeMap.put(task.getId(), newNode);
+    }
+
+    private void removeNode(Node<Task> node) {
+        final Node<Task> next = node.next;
+        final Node<Task> prev = node.prev;
+
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+
+        node.item = null;
+    }
+}
