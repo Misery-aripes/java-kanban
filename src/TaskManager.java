ff7d7791e40c0d502531369ffbf7d7ee7089b405
Misery <senya.stoletov.90@mail.ru>
2024-07-25 17:14:25 +0500

    методы отделены строками, что бы пройти автоматический тест

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,21 +2,38 @@ import java.util.List;
 
 public interface TaskManager {
     Task createTask(Task task);
+
     Task updateTask(Task task);
+
     List<Task> getTasks();
+
     void deleteTaskById(int taskId);
+
     Task getTask(int taskId);
+
     Epic createEpic(Epic epic);
+
     Epic updateEpic(Epic epic);
+
     List<Epic> getEpics();
+
     void deleteEpicById(int epicId);
+
     Epic getEpicById(int epicId);
+
     Subtask createSubtask(Subtask subtask);
+
     List<Subtask> getSubtasks();
+
     void deleteSubtaskById(int subtaskId);
+
     Subtask getSubtask(int subtaskId);
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
+
     void deleteAllTasks();
+
     List<Task> getHistory();
 }
